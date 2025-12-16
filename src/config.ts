5ea4f51d84c2757a66384d39b235642480befb26
renleihaokun <119118549+renleihaokun@users.noreply.github.com>
2025-12-16 13:20:16 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -173,7 +173,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		// 留空以使用默认 favicon
 		{
-		   src: '/favicon/icon.png',    // 图标文件路径
+		   src: '/favicon/favicon.ico',    // 图标文件路径
 		   theme: 'light',              // 可选，指定主题 'light' | 'dark'
 		   sizes: '32x32',              // 可选，图标大小
 		}
