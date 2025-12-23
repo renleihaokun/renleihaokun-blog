16f68f4481e68e326d1ef912fe77855c0d02b8a6
renleihaokun <119118549+renleihaokun@users.noreply.github.com>
2025-12-23 13:15:41 +0800

    Update and rename 提交这样的配置.json to 提交这样的配置.ts

---
+++ src/data/friends/提交这样的配置.ts
@@ -0,0 +1,10 @@
+export default [
+  {
+    id: 1,
+    title: "人类好困",
+    imgurl: "https://avatars.githubusercontent.com/u/119118549?v=4",
+    desc: "破站",
+    siteurl: "https://blog.haokun.me",
+    tags: ["blog"],
+  }
+]
