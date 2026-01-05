4d30159c651987a6061698b7e45ff729147ca1e6
renleihaokun <119118549+renleihaokun@users.noreply.github.com>
2026-01-05 18:58:25 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -194,7 +194,7 @@ export const siteConfig: SiteConfig = {
 		cjkFont: {
 			// 中日韩字体 - 作为回退字体
 			fontFamily: "LXGWNeoXiHeiPlus",
-			fontWeight: "500",
+			fontWeight: "700",
 			localFonts: ["LXGWNeoXiHeiPlus.ttf"],
 			enableCompress: true, // 启用字体子集优化，减少字体文件大小
 		},
