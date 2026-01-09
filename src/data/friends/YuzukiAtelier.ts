84274c2f1ed7896fc67db4eaae7d5661b6ca031d
renleihaokun <renleihaokun@gmail.com>
2026-01-09 22:08:58 +0800

    嘲讽

--- src/data/friends/YuzukiAtelier.ts
+++ src/data/friends/YuzukiAtelier.ts
@@ -1,10 +1,10 @@
 export default [
-  {
-    id: "yuzuki-atelier",
-    title: "摇月的魔法工坊",
-    imgurl: "https://YuzukiAtelier.com/head.webp",
-    desc: "记录了一些现代魔法的咏唱技法",
-    siteurl: "https://YuzukiAtelier.com",
-    tags: ["blog"], 
-  }
-] 
+	{
+		id: "yuzuki-atelier", //这里应该是数字啊喂
+		title: "摇月的魔法工坊",
+		imgurl: "https://YuzukiAtelier.com/head.webp",
+		desc: "记录了一些现代魔法的咏唱技法",
+		siteurl: "https://YuzukiAtelier.com",
+		tags: ["blog"],
+	},
+];
